d87c17449654c037b9ae8da1ae35846f10625f64
justanoval <163548474+justanoval@users.noreply.github.com>
2024-07-18 23:23:56 -0400

    Updated streamer-bot.gd
    It's referencing the right file now, but I still don't think it does anything.

--- streamer-bot.gd
+++ streamer-bot.gd
@@ -2,7 +2,7 @@
 extends EditorPlugin
 
 func _enter_tree() -> void:
-	add_custom_type("StreamerBot", "Node", preload("streamer-bot.gd"), preload("icon.jpg"))
+	add_custom_type("StreamerBot", "Node", preload("StreamerBot.gd"), preload("icon.jpg"))
 
 func _exit_tree() -> void:
 	remove_custom_type("StreamerBot")
